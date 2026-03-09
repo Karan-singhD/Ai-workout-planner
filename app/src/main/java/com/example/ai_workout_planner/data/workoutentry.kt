d61d6ba14786eac4c71f93b58fb46477b134021e
Gurkaran Singh <gurkarandyalsingh@gmail.com>
2026-03-09 12:32:28 +1100

    workout data structure

---
+++ app/src/main/java/com/example/ai_workout_planner/data/workoutentry.kt
@@ -0,0 +1,8 @@
+package com.example.aiworkoutplanner.data
+// format for excersice entry
+data class WorkoutEntry(
+    val exerciseName: String,
+    val sets: String,
+    val reps: String,
+    val weight: String
+)
